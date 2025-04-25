f07df06b5261f1df772d8dc1e6b2c745b1486a52
M.Usman Nadeem <ksam2473@gmail.com>
2025-04-25 22:42:43 +0500

    First Commit, Setup project

---
+++ FastCompete_Backend/resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+// Import Bootstrap's JavaScript |  Bootstrap core JavaScript and Popper.js (used for tooltips and popovers). | .bundle.min version includes Bootstrap’s core JS functionality along with the Popper.js library, which is necessary for certain Bootstrap components.
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+// Import Bootstrap 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Font Awesome's CSS
+import '@fortawesome/fontawesome-free/css/all.min.css';
